2ad8e26ee7f1226364815ccbc29e0a4ee12bd775
Tapialj <ljptapia@gmail.com>
2021-08-23 09:54:07 -0500

    javascript numbers practice

---
+++ Javascript/js/numbers.js
@@ -0,0 +1,30 @@
+var total = 0;
+total += 2;
+total += 3;
+total++;
+
+console.log("Total is " + total);
+
+
+var num1 = 0;
+var num2 = 16;
+var num3 = -0;
+var num4 = -16;
+
+console.log(num2 / num1);
+console.log(num4 / num1);
+console.log(num2 / num3);
+
+
+var text = "this is a string";
+var num5 = 2;
+var num6 = 10;
+
+console.log(text / num5);
+console.log(text / num5 + num6);
+
+alert("Max value: " + Number.MAX_VALUE +
+    "\nMin value: " + Number.MIN_VALUE +
+    "\nNegative infinity: " + Number.NEGATIVE_INFINITY +
+    "\nPositive infinity: " + Number.POSITIVE_INFINITY +
+    "\nNaN: " + Number.NaN);
