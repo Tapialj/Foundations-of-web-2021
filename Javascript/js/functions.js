f04cf1d5f1e8e04af17970c993c2b265ec4d5ec4
Tapialj <ljptapia@gmail.com>
2021-08-24 10:39:53 -0500

    function practice

---
+++ Javascript/js/functions.js
@@ -0,0 +1,20 @@
+function greeting()
+{
+    alert("Hello World!");
+}
+
+//greeting();
+
+function displayName(first, last)
+{
+    alert("Hello " + first + " " + last + "!");
+}
+
+/**
+displayName("Jarvis", "Johnson");
+displayName("Kurtis", "Conner");
+displayName(1, 2);
+displayName("Danny", "Gonzalez", "Drew");
+displayName("Gooden");
+*/
+
